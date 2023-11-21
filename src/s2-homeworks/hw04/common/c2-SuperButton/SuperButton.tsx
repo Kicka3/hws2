6c274496011fb6bd7a2f1563e24a471846667204
Kirill <kirill_ozornin@mail.ru>
2023-11-21 18:45:59 +0200

    add new inputs and checkboxes

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,10 +19,10 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
 ) => {
     const finalClassName = s.button
         // + (disabled
-        //         ? ...
+        //         ?
         //         : xType === 'red'
         //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        // + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
