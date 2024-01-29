30426185458c53ba1651d3756a91b3aac16352b2
Kirill <kirill_ozornin@mail.ru>
2024-01-29 21:13:00 +0500

    hw 10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,11 @@
 import s from './Loader.module.css'
+import load from '../hw10/Spinner.svg'
 
-export const Loader = () => <div className={s.loader}/>
+// export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+    return (<div className={s.loader}>
+        <img src={load}/>
+    </div>
+    )
+
+}
